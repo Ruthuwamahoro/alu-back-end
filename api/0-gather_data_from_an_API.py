0c03c69c5cced27c61788db9b7ac9d4150cec29a
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-07-09 21:32:15 +0000

    adding

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,33 +1,14 @@
 #!/usr/bin/python3
-"""Python script that returns the value of an employee's TODO list"""
-import json
+"""Returns to-do list information for a given employee ID."""
+import requests
 import sys
-import urllib.request
 
-if __name__ == '__main__':
-    """Converting the arguments into integers"""
-    EMPLOYEE_ID = int(sys.argv[1])
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    """Using URL to retrieve data about employees"""
-    BASE_URL = "https://jsonplaceholder.typicode.com"
-    employee_url = "{}/users/{}".format(BASE_URL, EMPLOYEE_ID)
-    todo_url = "{}/todos?userId={}".format(BASE_URL, EMPLOYEE_ID)
-
-    """Opening employee URL using urllib module"""
-    with urllib.request.urlopen(employee_url) as f:
-        employee_info = json.loads(f.read().decode('utf-8'))
-
-    with urllib.request.urlopen(todo_url) as response:
-        data = json.loads(response.read().decode('utf-8'))
-
-    """Retrieving data"""
-    EMPLOYEE_NAME = employee_info["name"]
-    TOTAL_NUMBER_OF_TASKS = len(data)
-    NUMBER_OF_DONE_TASKS = len([task for task in data if task["completed"]])
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-
-    for task in data:
-        if task["completed"]:
-            print("\t{}".format(task["title"]))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
