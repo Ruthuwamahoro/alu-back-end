2007dc518ac7143955541ef27756903f9d5b72bd
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-07-09 17:17:17 +0000

    adding

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
 """Exports to-do list information for a given employee ID to JSON format."""
 import json
-import requests
+import urllib.parse
+import urllib.request
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    user_id = sys.argv[1] 
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_endpoint = "users/{}".format(user_id)
+    todos_endpoint = "todos"
+
+    user_url = urllib.parse.urljoin(base_url, user_endpoint)
+    todos_url = urllib.parse.urljoin(base_url, todos_endpoint)
+
+    with urllib.request.urlopen(user_url) as response:
+        user = json.loads(response.read().decode())
+        username = user.get("username")
+
+    todos_params = {"userId": user_id}
+    todos_url_with_params = todos_url + "?" + urllib.parse.urlencode(todos_params)
+    with urllib.request.urlopen(todos_url_with_params) as response:
+        todos = json.loads(response.read().decode())
 
     with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
+        json.dump({user_id: [
+            {
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": username
-            } for t in todos]}, jsonfile)
+            }
+            for t in todos
+        ]}, jsonfile)
